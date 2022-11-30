87c5d24f13573ead05984f12fa97dcef45f09864
x0s <6331909+x0s@users.noreply.github.com>
2022-11-30 21:13:13 +0100

    Simplify config loading at testing, moving from EAFP to LBYL

--- advent_of_code/config.py
+++ advent_of_code/config.py
@@ -7,17 +7,15 @@ from contextlib import contextmanager
 from pathlib import Path
 
 
-try:
+# If the tests are not running, we load configuration(token)
+if 'unittest' not in sys.modules:
     # Read config to get connexion settings (ie: token)
     with open(Path(__file__).resolve().parent / "config.toml", "rb") as f:
         CONFIG = tomllib.load(f)
-except FileNotFoundError:
-    # No problem if we are running tests, elsewhere we reraise
-    if 'unittest' not in sys.modules:
-        raise
 
 @contextmanager
 def get_input(year: int, day: int) -> Iterator[str]:
+    """Get input data from remote aoc server for a given year/day"""
     url = f"https://adventofcode.com/{year}/day/{day}/input"
     with requests.get(url, cookies={"session": CONFIG['token']}) as input_raw:
         yield input_raw.text
