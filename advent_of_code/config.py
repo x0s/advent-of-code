e8b304504ec001022db9d33776d4b547e2128adb
x0s <6331909+x0s@users.noreply.github.com>
2022-11-30 16:05:08 +0100

    read config (with token) only when not running tests

--- advent_of_code/config.py
+++ advent_of_code/config.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 import tomllib
 from typing import Iterator
 
@@ -6,9 +7,14 @@ from contextlib import contextmanager
 from pathlib import Path
 
 
-# Read config to get connexion settings (ie: token)
-with open(Path(__file__).resolve().parent / "config.toml", "rb") as f:
-    CONFIG = tomllib.load(f)
+try:
+    # Read config to get connexion settings (ie: token)
+    with open(Path(__file__).resolve().parent / "config.toml", "rb") as f:
+        CONFIG = tomllib.load(f)
+except FileNotFoundError:
+    # No problem if we are running tests, elsewhere we reraise
+    if 'unittest' not in sys.modules:
+        raise
 
 @contextmanager
 def get_input(year: int, day: int) -> Iterator[str]:
