3aca57057bc13fe851a563f2928a73c0a0ddc7ec
x0s <6331909+x0s@users.noreply.github.com>
2022-12-01 00:33:43 +0100

    Refactor how we get the token and Test whole config module

--- advent_of_code/config.py
+++ advent_of_code/config.py
@@ -1,5 +1,4 @@
 import requests
-import sys
 import tomllib
 from typing import Iterator
 
@@ -7,15 +6,18 @@ from contextlib import contextmanager
 from pathlib import Path
 
 
-# If the tests are not running, we load configuration(token)
-if 'unittest' not in sys.modules:
+
+def get_token(config_file: str="config.toml") -> str:
+    """Get token from TOML config file"""
     # Read config to get connexion settings (ie: token)
-    with open(Path(__file__).resolve().parent / "config.toml", "rb") as f:
+    with open(Path(__file__).resolve().parent / config_file, "rb") as f:
         CONFIG = tomllib.load(f)
+    return CONFIG['token']
+
 
 @contextmanager
 def get_input(year: int, day: int) -> Iterator[str]:
     """Get input data from remote aoc server for a given year/day"""
     url = f"https://adventofcode.com/{year}/day/{day}/input"
-    with requests.get(url, cookies={"session": CONFIG['token']}) as input_raw:
+    with requests.get(url, cookies={"session": get_token()}) as input_raw:
         yield input_raw.text
---
+++ tests/test_config.py
@@ -0,0 +1,46 @@
+from contextlib import contextmanager
+from unittest import TestCase
+from unittest.mock import patch, mock_open
+
+from advent_of_code.config import (get_token,
+                                   get_input)
+
+
+# inspired from https://stackoverflow.com/a/28507806/3581903
+@contextmanager
+def mocked_requests_get(url, *args, **kwargs):
+    class MockResponse:
+        def __init__(self, text, status_code):
+            self.text = text
+            self.status_code = status_code
+
+    if url == 'https://adventofcode.com/2021/day/13/input':
+        raw_text_fixture = "forward 5\ndown 5\nforward 8\nup 3"
+        yield MockResponse(raw_text_fixture, 200)
+
+
+class TestExamples(TestCase):
+
+    config_token_fixture = "ru=2315512c2123c3312q351c56q2; session=5654512c785c21b"
+    config_toml_fixture = f"""token = \"{config_token_fixture}\"""".encode('utf-8') # should be binary
+
+    @classmethod
+    def setUpClass(cls):
+        
+        # We set up the same fixture input for all test case(part 1 and 2), py>=3.11
+        cls.enterClassContext(
+            patch('builtins.open', mock_open(read_data=cls.config_toml_fixture)))
+        cls.enterClassContext(
+             patch('advent_of_code.config.requests.get', side_effect=mocked_requests_get))
+
+        super().setUpClass()
+
+    def test_get_token(self):
+        """Test if token is correctly retrieved from TOML config file"""
+        self.assertEqual(get_token(), self.config_token_fixture)
+    
+    def test_get_input(self):
+        """Test if token is correctly retrieved from TOML config file"""
+
+        with get_input(year=2021, day=13) as raw_text:
+            self.assertEqual(raw_text, "forward 5\ndown 5\nforward 8\nup 3")
