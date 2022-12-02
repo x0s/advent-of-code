b39e556589fc955761757fc0dc832976bbbcab01
x0s <guillaume.fidanza@gmail.com>
2022-12-02 21:44:33 +0100

    Refactor [2022/01] with class and Fix import issue (script<->tests collision)

--- advent_of_code/year_2022/day01/part_2.py
+++ advent_of_code/year_2022/day01/part_2.py
@@ -1,17 +1,19 @@
 from advent_of_code.config import get_input
 from advent_of_code.logging import log
-from .part_1 import sum_per_elf
+from advent_of_code.year_2022.day01.part_1 import SolutionOne
 
-   
-def part_two(input_raw: str) -> int:
-    """Find total Calories carried by the three Elves carrying the most"""
-    return sum(sorted(sum_per_elf(input_raw))[-3:])
+
+class SolutionTwo:
+    @classmethod
+    def process(cls, input_raw: str) -> int:
+        """Find total Calories carried by the three Elves carrying the most"""
+        return sum(sorted(SolutionOne.sum_per_elf(input_raw))[-3:])
 
 
 def main() -> int:
     with get_input(year=2022, day=1) as input_raw:
        
-        total_calories = part_two(input_raw)
+        total_calories = SolutionTwo.process(input_raw)
 
         log.info(f"Total calories carried by the 3 Elves carrying the most = {total_calories}")
 
