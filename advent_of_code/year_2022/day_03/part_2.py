2f64d94fdfd6e560ab9c7d63b5fd8201d3102e34
x0s <6331909+x0s@users.noreply.github.com>
2022-12-03 19:26:20 +0100

    Add solutions for [2022/03] + tests

---
+++ advent_of_code/year_2022/day_03/part_2.py
@@ -0,0 +1,26 @@
+from advent_of_code.config import get_input
+from advent_of_code.logging import log
+from advent_of_code.year_2022.day_03.part_1 import SolutionOne
+
+
+class SolutionTwo:
+    
+    @classmethod
+    def process(self, input_raw: str) -> int:
+        """Compute the priority sums"""
+        sacks = input_raw.strip().split('\n')
+        return sum([SolutionOne.priority((set(sacks[i]) & set(sacks[i+1]) & set(sacks[i+2])).pop()) for i,_ in enumerate(sacks) if i%3==0 ])
+
+
+def main() -> int:
+    with get_input(year=2022, day=3) as input_raw:
+       
+        priority_sum = SolutionTwo.process(input_raw)
+
+        log.info(f"Priority sum = {priority_sum}")
+
+        # we return what's been asked
+        return priority_sum
+
+if __name__ == "__main__":
+    main()
