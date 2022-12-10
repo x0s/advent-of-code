35ad604faf25fd1540840fb1b3a9c1b8b52e1c5f
x0s <6331909+x0s@users.noreply.github.com>
2022-12-10 15:53:10 +0100

    Refactor Test utils so we can patch different inputs for part 1 and 2

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,4 @@
+from typing import Iterator
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -7,9 +8,12 @@ class TestExamplesMain(TestCase):
     def setUpClassAndPatch(cls, year, day, mocked_get_input):
         
         # We set up the same fixture input for all test case(part 1 and 2), py>=3.11
-        cls.enterClassContext(
-            patch(f'advent_of_code.year_{year}.day_{day:02}.part_1.get_input', side_effect=mocked_get_input))
-        cls.enterClassContext(
-            patch(f'advent_of_code.year_{year}.day_{day:02}.part_2.get_input', side_effect=mocked_get_input))
+        cls.patchGetInput(year, day, 1, mocked_get_input)
+        cls.patchGetInput(year, day, 2, mocked_get_input)
 
         super().setUpClass()
+    
+    @classmethod
+    def patchGetInput(cls, year: int, day:int, part: int, mocked_get_input: Iterator):
+        cls.enterClassContext(
+            patch(f'advent_of_code.year_{year}.day_{day:02}.part_{part}.get_input', side_effect=mocked_get_input))
