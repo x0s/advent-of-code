158f1fd3c4f40ac206ff94c7d04630542ad86471
x0s <6331909+x0s@users.noreply.github.com>
2022-12-14 18:28:13 +0100

    Refactor parts 1 & 2 in 2022/14

--- advent_of_code/year_2022/day_14/part_1.py
+++ advent_of_code/year_2022/day_14/part_1.py
@@ -33,7 +33,7 @@ class Point:
                         {from_ + Point(i, 0) for i in range(to_.x - from_.x + 1)}))
 
 
-class SolutionOne:
+class Cave:
 
     def __init__(self, input_raw: str, floor_offset: int | float = float('inf')) -> None:
         paths = [path.split(' -> ') for path in input_raw.splitlines()]
@@ -51,34 +51,38 @@ class SolutionOne:
              self.loop_max_y += floor_offset 
         self.floor_y = self.blocked.max_y + floor_offset
 
-    @staticmethod
-    def is_blocked(sand: Point, blocked: PointSet, floor_y: float | int) -> bool:
-        return (sand in blocked) or (sand.y >= floor_y)
-
-    @classmethod
-    def pour(cls, sand: Point, blocked: PointSet, floor_y: int | float) -> Point | None:
-        if   not cls.is_blocked((new := sand + Point(0,  1)), blocked, floor_y): return new
-        elif not cls.is_blocked((new := sand + Point(-1, 1)), blocked, floor_y): return new
-        elif not cls.is_blocked((new := sand + Point(+1, 1)), blocked, floor_y): return new
-        else: blocked.update({sand})
-
-
-    def process(self) -> int:
-        
-        sand = Point(500, 0)
     
+    def is_blocked(self, sand: Point) -> bool:
+        return (sand in self.blocked) or (sand.y >= self.floor_y)
+
+    def next_coord(self, sand: Point) -> Point | None:
+        if   not self.is_blocked((new := sand + Point( 0, 1))): return new
+        elif not self.is_blocked((new := sand + Point(-1, 1))): return new
+        elif not self.is_blocked((new := sand + Point(+1, 1))): return new
+        else: self.blocked.update({sand})
+    
+    def pour(self):
+        sand = Point(500, 0)
         while sand is not None and sand.y < self.loop_max_y:
             # trying to pour, if blocked, try to start with new sand
-            if (sand := self.pour(sand, self.blocked, self.floor_y)) is None:
+            if (sand := self.next_coord(sand)) is None:
                 if(sand := Point(500,0)) in self.blocked: break
-
+    @property
+    def n_resting(self):
         return len(self.blocked) - self.rock_len
+
+class SolutionOne:
+    @classmethod
+    def process(self, input_raw) -> int:
+        cave = Cave(input_raw)
+        cave.pour()
+        return cave.n_resting
 
 
 def main() -> int:
     with get_input(year=2022, day=14) as input_raw:
        
-        total = SolutionOne(input_raw).process()
+        total = SolutionOne.process(input_raw)
 
         log.info(f"How many units of sand come to rest before sand starts flowing into the abyss below?= {total}")
 
--- advent_of_code/year_2022/day_14/part_2.py
+++ advent_of_code/year_2022/day_14/part_2.py
@@ -1,12 +1,19 @@
 from advent_of_code.config import get_input
 from advent_of_code.logging import log
-from advent_of_code.year_2022.day_14.part_1 import SolutionOne
+from advent_of_code.year_2022.day_14.part_1 import SolutionOne,Cave
 
+
+class SolutionTwo(SolutionOne):
+    @classmethod
+    def process(self, input_raw) -> int:
+        cave = Cave(input_raw, floor_offset=2)
+        cave.pour()
+        return cave.n_resting
 
 def main() -> int:
     with get_input(year=2022, day=14) as input_raw:
        
-        total = SolutionOne(input_raw, floor_offset=2).process()
+        total = SolutionTwo.process(input_raw)
 
         log.info(f"How many units of sand come to rest?= {total}")
 
