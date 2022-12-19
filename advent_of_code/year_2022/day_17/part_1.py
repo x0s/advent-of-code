69f23e75d88900a372c67f13eaf8576016830d82
x0s <6331909+x0s@users.noreply.github.com>
2022-12-19 10:42:44 +0100

    Refactor and complete Solutions for [2022/17] boths parts + Tests

--- advent_of_code/year_2022/day_17/part_1.py
+++ advent_of_code/year_2022/day_17/part_1.py
@@ -1,6 +1,4 @@
 
-import re
-
 from itertools import cycle, zip_longest
 from typing import Iterable
 
@@ -8,6 +6,13 @@ from typing import Iterable
 from advent_of_code.config import get_input
 from advent_of_code.logging import log
 
+
+# (rock_id, move_id) -> (rock_num, height)
+RecordType = dict[tuple[int], tuple[int]]
+# Store blocked positions in tower
+TowerType = set[tuple]
+# A rock is represented by the coordinates of its pixels
+RockType = set[tuple]
 
 class SolutionOne:
 
@@ -30,74 +35,87 @@ class SolutionOne:
 ##
 """
 
+    directions = {'>': (+1,  0), # right
+                  '<': (-1,  0), # left
+                  '|': ( 0, -1)} # down
+
     @staticmethod
-    def tuple_add(a, b):
+    def tuple_add(a: tuple, b: tuple) -> tuple:
         return (a[0] + b[0]), (a[1] + b[1])
 
-    def get_moves(input_raw: str):
+    @classmethod
+    def get_direction(cls, input_raw: str) -> Iterable[tuple[int, tuple]]:
         jets = [jet for jet in input_raw.rstrip()]
         # insert "down":| move every 'left':> or 'right':< moves
-        moves = cycle([move for move_with_down in zip_longest(jets, [], fillvalue='|') for move in move_with_down if move])
-        for move in moves:
-            yield move
+        moves = cycle(enumerate([move for move_with_down in zip_longest(jets, [], fillvalue='|')
+                                      for move in move_with_down if move]))
+        for index, move in moves:
+            yield index,cls.directions[move]
 
     @staticmethod
-    def is_blocked(rock, tower):
-        return (bool(tower & set(rock)) or 
+    def is_blocked(rock: RockType, tower: TowerType) -> bool:
+        return (bool(tower & rock) or 
                 not all((0 <= x < 7 and y >= 0) for (x,y) in rock))
 
     @staticmethod
-    def get_height(tower):
-        return  max(tower, key=lambda x_y: x_y[1])[1] + 1
+    def get_height(tower: TowerType) -> int:
+        return  (max(tower, key=lambda x_y: x_y[1])[1] + 1) if len(tower) > 0 else 0
 
     @classmethod
-    def process(cls, input_raw) -> int:
-        directions = {'>': (+1,  0), # right
-                      '<': (-1,  0), # left
-                      '|': ( 0, -1)} # down
+    def process(cls, input_raw: str, n_rocks: int) -> int:
         
         rocks = [{(i, j) for j,line in enumerate(rock.splitlines()[::-1]) 
                  for i,c in enumerate(line) if c =='#'} 
                  for rock in cls.rocks_raw.split('\n\n')]
 
-        gap_down = 3                        # distance to the highest rock(floor if first rock)
+        gap_down = 3
         gap_left = 2
-        n_rocks =  10000                    # number of rocks about to fall
         offset_start = (gap_left, gap_down)
-        tower = set()
-        moves = cls.get_moves(input_raw) # cycle
+        tower : TowerType = set()
+        records: RecordType = {}
+        iter_direction = cls.get_direction(input_raw)
 
-        for i,rock in enumerate(cycle(rocks)):
-            if i >= n_rocks: break
-            new_rock = True
+        for rock_num,(rock_id, rock) in enumerate(cycle(enumerate(rocks))):
+            if rock_num >= n_rocks: break
 
-            not_blocked = True
-            offset = (gap_left, 3+1+max(tower, key=lambda x_y: x_y[1])[1]) if i>0 else offset_start
-            rock = [cls.tuple_add(pixel, offset) for pixel in rock]
+            offset = (gap_left, 3+1+max(tower, key=lambda x_y: x_y[1])[1]) if rock_num>0 else offset_start
+            rock = {cls.tuple_add(pixel, offset) for pixel in rock}
 
-            while not_blocked:
-
-                if (direction := directions[next(moves)])[1] == -1 and new_rock == -1:
-                    direction = directions[next(moves)] # first move should be left or right
-                    new_rock = False
-
+            while True:
+                move_id, direction = next(iter_direction)
                 rock_prev = rock
-                rock = [cls.tuple_add(pixel, direction) for pixel in rock]
+                # Let's see where <would> be the next position if we really make this move
+                rock = {cls.tuple_add(pixel, direction) for pixel in rock}
 
-                if direction[1] == -1 and cls.is_blocked(rock, tower): # if going down and blocked
-                    not_blocked = False
-                    tower.update(rock_prev)
-                elif abs(direction[0]) == 1 and cls.is_blocked(rock, tower): # going laterally and blocked
-                    rock = rock_prev
-                    continue
+                # Have we already seen this rock moving this way ?
+                if (key := (rock_id, move_id)) in records:
+                    rock_num_prev, height = records[key]
+                    rock_num_delta = rock_num - rock_num_prev
+                    # Can we see this pattern repeating until all rocks would have fallen ?
+                    if rock_num % rock_num_delta == n_rocks % rock_num_delta:
+                        log.info(f"Cycle consisting of {rock_num_delta} rocks")
+                        height_delta = cls.get_height(tower) - height
+                        n_rocks_left = n_rocks - rock_num
+                        cycles_left = (n_rocks_left // rock_num_delta) + 1
+                        return height + height_delta * cycles_left
+                else:
+                    records[key] = (rock_num, cls.get_height(tower))
+
+                if cls.is_blocked(rock, tower):
+                    if direction[1] == -1:       # if going down
+                        tower.update(rock_prev)
+                        break
+                    elif abs(direction[0]) == 1: # if going laterally
+                        rock = rock_prev
+                        continue
 
         return cls.get_height(tower)
 
 
 def main() -> int:
-    with get_input(year=2022, day=16) as input_raw:
+    with get_input(year=2022, day=17) as input_raw:
        
-        total = SolutionOne.process(input_raw)
+        total = SolutionOne.process(input_raw, n_rocks=2022)
 
         log.info(f"How many units tall will the tower of rocks be after 2022 rocks have stopped falling? {total}")
 
