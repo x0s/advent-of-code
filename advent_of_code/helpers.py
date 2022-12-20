f53fd2abb51e2953d8fa4a8e63858ec1f81198b7
x0s <guillaume.fidanza@gmail.com>
2022-12-20 13:23:25 +0100

    Add helper to compute addition between tuples of any length

--- advent_of_code/helpers.py
+++ advent_of_code/helpers.py
@@ -2,9 +2,11 @@ import logging
 
 from collections.abc import Iterable, Iterator
 from contextlib import contextmanager
+from operator import add
 from typing import TypeVar
 
 YieldValue = TypeVar('YieldValue')
+AnyTupleNumber = tuple[int | float, ...]
 
 def takewhile_inc(max_value: float, iterable: Iterable[YieldValue]) -> Iterator[YieldValue]:
     # takewhile inclusive (x<5, [1,4,9,2]) --> 1 4 9
@@ -24,3 +26,6 @@ def logging_level(level: str | int) -> Iterator[None]:
         yield
     finally:
         logging.disable('CRITICAL')
+
+def tuple_add(a: AnyTupleNumber, b: AnyTupleNumber) -> AnyTupleNumber:
+    return tuple(map(add, a, b))
