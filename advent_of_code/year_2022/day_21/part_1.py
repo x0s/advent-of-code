428b87e2f8cf722ed0be7da56a3e101f3ee62ba9
x0s <6331909+x0s@users.noreply.github.com>
2023-02-28 17:45:14 +0100

    Remove example raw input in [2022/21-1], now working with remote one

--- advent_of_code/year_2022/day_21/part_1.py
+++ advent_of_code/year_2022/day_21/part_1.py
@@ -37,8 +37,6 @@ class SolutionOne:
 def main() -> int:
     with get_input(year=2022, day=21) as input_raw:
 
-        input_raw = 'root: pppw + sjmn\ndbpl: 5\ncczh: sllz + lgvd\nzczc: 2\nptdq: humn - dvpt\ndvpt: 3\nlfqf: 4\nhumn: 5\nljgn: 2\nsjmn: drzm * dbpl\nsllz: 4\npppw: cczh / lfqf\nlgvd: ljgn * ptdq\ndrzm: hmdt - zczc\nhmdt: 32\n'
-
         total = SolutionOne().process(input_raw)
 
         log.info(f"What number will the monkey named root yell? {total}")
